781235f7c054cd266889e73f102f73c08a499e32
arturoloureiro <arthurloureirolima@gmail.com>
2026-04-19 16:29:27 -0300

    Prosseguir com mais opções de filtros.

--- app/src/main/java/com/example/compose_criar_produto/MainActivity.kt
+++ app/src/main/java/com/example/compose_criar_produto/MainActivity.kt
@@ -1,36 +1,31 @@
 package com.example.compose_criar_produto
 
-import ExpandingTextField
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -40,23 +35,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.draw.paint
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.compose_criar_produto.ui.components.ButtonCustomized
-import com.example.compose_criar_produto.ui.components.FixedTextField
-import com.example.compose_criar_produto.ui.theme.BgDefault
+import com.example.compose_criar_produto.ui.components.ButtonFilter
+import com.example.compose_criar_produto.ui.components.ButtonMoreCustomizable
+import com.example.compose_criar_produto.ui.components.Categorias
 import com.example.compose_criar_produto.ui.theme.ComposecriarprodutoTheme
+import com.example.compose_criar_produto.ui.components.CalculadoraCategoria
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -102,82 +94,65 @@ fun Tela() {
                 .verticalScroll(scrollState)
 
         ) {
-            Text(
-                "Adicionar Produto",
-                Modifier.width(200.dp),
-                fontFamily = libreCaslonDisplay,
-                fontSize = 50.sp,
-                lineHeight = 40.sp,
-                textAlign = TextAlign.Center
-            )
-            Spacer(Modifier.height(50.dp))
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
+            var mostrarCategorias by remember { mutableStateOf(false) }
+            Row(modifier = Modifier.fillMaxWidth() .height(40.dp)) {
+                ButtonFilter(
+                    text = "Filtros",
+                    onClick = { mostrarCategorias = !mostrarCategorias }
+                )
+            }
+
+            AnimatedVisibility(
+                visible = ButtonFilter.statusCategoriasAbertas,
+                enter = expandVertically(
+                    expandFrom = Alignment.Top,
+                    animationSpec = tween(durationMillis = 300)
+                ) + fadeIn(animationSpec = tween(durationMillis = 300)),
+                exit = shrinkVertically(
+                    shrinkTowards = Alignment.Top,
+                    animationSpec = tween(durationMillis = 300)
+                ) + fadeOut(animationSpec = tween(durationMillis = 300))
+            ) {
+                Column {
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Categorias(
+                        categorias = listOf("Têxteis", "Cerâmica", "Entalhe"),
+                    ) { categoria ->
+                        val larguraDinamica by remember(categoria) {
+                            mutableStateOf(CalculadoraCategoria.calcularLargura(categoria))
+                        }
+
+                        ButtonMoreCustomizable(
+                            text = categoria,
+                            textColor = btColor,
+                            containerColor = bgColor,
+                            borderColor = btColor,
+                            widthButton = larguraDinamica,
+                            heightButton = 40.0,
+                            modifier = Modifier,
+                            onClick = { println("Clicou em $categoria") }
+                        )
+                    }
+                }
+            }
+
+            Box(
+                contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .fillMaxWidth(1f)
-                    .wrapContentHeight()
+                    .height(50.dp)
                     .background(bgColor)
 
             ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth(0.83f)
-                        .aspectRatio(348f / 284f)
-                        .paint(
-                            painterResource(id = R.drawable.placeholder),
-                            contentScale = ContentScale.Crop
-                        ),
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        ButtonCustomized(
-                            "escolher imagem",
-                            Color.Black,
-                            BgDefault,
-                            Color.Black
-                        ) {} // Como o parâmetro do onClick
-                    }
-                }
-
-                Spacer(Modifier.height(20.dp))
-                FixedTextField(
-                    text = textName,
-                    textColor = bgColor,
-                    onTextChange = { textName = it },
-                    placeholder = "Nome",
-                    containerColor = btColor,
-                    borderColor = btColor
+                Text(
+                    "Descubra",
+                    Modifier.width(200.dp),
+                    fontFamily = libreCaslonDisplay,
+                    fontSize = 40.sp,
+                    textAlign = TextAlign.Center
                 )
-                Spacer(Modifier.height(20.dp))
-                ExpandingTextField(
-                    text = textDesc1,
-                    textColor = Color.Black,
-                    onTextChange = { textDesc1 = it },
-                    placeholder = "Descrição",
-                    containerColor = bgColor,
-                    borderColor = btColor
-                )
-                Spacer(Modifier.height(20.dp))
-                ExpandingTextField(
-                    text = textDesc2,
-                    textColor = Color.Black,
-                    onTextChange = { textDesc2 = it },
-                    placeholder = "Descrição adicional (opicional)",
-                    containerColor = bgColor,
-                    borderColor = btColor
-                )
-                Spacer(Modifier.height(20.dp))
-                ButtonCustomized(
-                    text = "Adicionar",
-                    textColor = bgColor,
-                    containerColor = btColor,
-                    borderColor = btColor
-                ) {}
             }
+
 
         }
     }
---
+++ app/src/main/java/com/example/compose_criar_produto/ui/components/AdaptiveLine.kt
@@ -0,0 +1,55 @@
+package com.example.compose_criar_produto.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kotlin.math.max
+
+@Composable
+fun AdaptiveLine (
+    modifier: Modifier = Modifier,
+    horizontalSpacing: Dp = 8.dp,
+    verticalSpacing: Dp = 8.dp,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        content = content,
+        modifier = modifier
+    ) { measurables, constraints ->
+
+        val looseConstraints = constraints.copy(minWidth = 0, minHeight = 0)
+        val placeables = measurables.map { it.measure(looseConstraints) }
+
+        var currentX = 0
+        var currentY = 0
+        var rowHeight = 0
+        val placeablePositions = mutableListOf<Pair<Int, Int>>()
+
+        placeables.forEach { placeable ->
+
+            if (currentX + placeable.width > constraints.maxWidth) {
+                currentX = 0
+                currentY += rowHeight + verticalSpacing.roundToPx()
+                rowHeight = 0
+            }
+
+            placeablePositions.add(Pair(currentX, currentY))
+
+            rowHeight = max(rowHeight, placeable.height)
+
+            currentX += placeable.width + horizontalSpacing.roundToPx()
+        }
+
+        layout(
+            width = constraints.maxWidth,
+            height = currentY + rowHeight
+        ) {
+            placeables.forEachIndexed { index, placeable ->
+                val (x, y) = placeablePositions[index]
+                placeable.placeRelative(x = x, y = y)
+            }
+        }
+    }
+}
